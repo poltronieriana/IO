ed761bdc15afcc45c6b7047e30ffb75ad2b28c44
Graziela <grazielaespindola82@gmail.com>
2025-11-28 20:57:02 -0300

    feat: adicionando estrutura

---
+++ src/processo.py
@@ -0,0 +1,14 @@
+class Processo:
+
+
+    def __init__(self, pid):
+        
+    def executa_processo(self):
+        pass
+
+    def backup_processo(self):
+        pass
+
+    def restaura_processo(self):
+        pass
+
